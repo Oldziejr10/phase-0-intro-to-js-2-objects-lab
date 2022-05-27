7cf2ecd1010312aed5cddfce43faff885b0cd705
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-05-26 20:54:11 -0400

    commit tree 1ceb51d82bac694c5e5d3458decd7537125acd7e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, _streetAddress, value) {
+    
+    const newEmployee = { ...employee, streetAddress: value };
+    
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[key] = value;
+
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) { 
+
+    const newEmployee = {...employee}
+
+    delete newEmployee[key];
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key];
+
+    return employee;
+    
+}
+const employee = { 
+    name: 'Rafal',
+};
+
+updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway');
+deleteFromEmployeeByKey(employee, 'name')
+destructivelyDeleteFromEmployeeByKey(employee, 'name')
